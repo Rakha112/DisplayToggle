3a8394fb9662aa256b618dbe4529c243dc414c30
Rakha Wibowo <rakhawibowo1998@gmail.com>
2025-09-18 14:11:25 +0700

    Initial Commit

---
+++ DisplayToggle/DisplayToggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisplayToggleApp.swift
+//  DisplayToggle
+//
+//  Created by Rakha Wibowo on 18/09/25.
+//
+
+import SwiftUI
+
+@main
+struct DisplayToggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
